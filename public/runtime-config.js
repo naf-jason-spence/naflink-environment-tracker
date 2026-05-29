a223b7195ab49caa8a0e7e448876a2abb2675688
Jason Spence <jason.spence@nafinc.com>
2026-05-29 17:42:02 -0500

    remove token prompt from ui

---
+++ public/runtime-config.js
@@ -0,0 +1,7 @@
+window.__ENVTRACKER_CONFIG__ = {
+  // Set via deployment process. Do not commit real secrets.
+  dispatchToken: '',
+  owner: 'NAF-Tech',
+  repo: 'naflink-environment-tracker',
+  ref: 'develop'
+};
